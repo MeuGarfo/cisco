dc02e9d1bbe1a7c6000f7616cd78871e82914471
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-02-01 16:02:01 -0200

    M  bin/export.php

--- bin/export.php
+++ bin/export.php
@@ -1,5 +1,7 @@
 <?php
+require './basic/basic.php';
+system("clear");
 $filename=ROOT.'backup.sql';
-print 'exportando DB para o arquivo backup.sql...'.PHP_EOL;
-system('mysqldump -u root -p cisco > "'.ROOT.'backup.sql"');
+print 'Disite a senha do seu banco de dados:'.PHP_EOL;
+system('mysqldump -v -u root -p cisco > "'.ROOT.'backup.sql"');
 print 'banco de dados exportado com sucesso!'.PHP_EOL;
