aae2b2bf835999c2aa9be3cba7e13bd89d898d32
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-02-03 10:16:30 -0200

    M  view/form/dois.php

--- view/form/dois.php
+++ view/form/dois.php
@@ -1,5 +1,5 @@
 <form class="" action="dois" method="GET">
-    <input type="text" name="sitea" value=""><br>
-    <input type="text" name="siteb" value=""><br>
+    <input type="text" name="sitea" value="" placeholder="site a"><br>
+    <input type="text" name="siteb" value="" placeholder="site b"><br>
     <button type="submit">Buscar</button>
 </form>
