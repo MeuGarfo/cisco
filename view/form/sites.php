9b895698a7a9064f7a0449b4c7743e58295f99ad
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-02-18 15:30:09 -0300

    M  view/form/sites.php

--- view/form/sites.php
+++ view/form/sites.php
@@ -1,6 +1,6 @@
 <form class="" action="sites" method="GET" id="sitesFrm">
     <input type="hidden" name="dia" value="30">
-    <textarea onkeyup="inputKeyUp(event)" name="sites" rows="8" cols="80" placeholder="Digite um site por linha"><?php if(isset($sitesValue)){e($sitesValue);} ?></textarea><br>
+    <textarea onkeyup="inputKeyUp(event)" name="sites" rows="8" cols="80" placeholder="Digite um site por linha (ex: google.com)"><?php if(isset($sitesValue)){e($sitesValue);} ?></textarea><br>
     <button type="submit">Buscar</button>
 </form>
 <script type="text/javascript">
