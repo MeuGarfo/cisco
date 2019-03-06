82687c3926bbbb56054ed11d02eff209c52b6779
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-03-06 16:17:58 -0300

    M  .gitignore M  basic/basic.json M  basic/basic.php M  basic/getbasic/controller/controller.php M  basic/getbasic/db/README.md M  basic/getbasic/db/composer.json M  basic/getbasic/db/composer.lock M  basic/getbasic/db/db.php M  basic/getbasic/db/vendor/autoload.php M  basic/getbasic/db/vendor/catfan/medoo/.gitattributes M  basic/getbasic/db/vendor/catfan/medoo/.github/ISSUE_TEMPLATE/bug_report.md M  basic/getbasic/db/vendor/catfan/medoo/.gitignore M  basic/getbasic/db/vendor/catfan/medoo/LICENSE.md M  basic/getbasic/db/vendor/catfan/medoo/README.md M  basic/getbasic/db/vendor/catfan/medoo/composer.json M  basic/getbasic/db/vendor/catfan/medoo/src/Medoo.php M  basic/getbasic/db/vendor/composer/ClassLoader.php M  basic/getbasic/db/vendor/composer/LICENSE M  basic/getbasic/db/vendor/composer/autoload_classmap.php M  basic/getbasic/db/vendor/composer/autoload_namespaces.php M  basic/getbasic/db/vendor/composer/autoload_psr4.php M  basic/getbasic/db/vendor/composer/autoload_real.php M  basic/getbasic/db/vendor/composer/autoload_static.php M  basic/getbasic/db/vendor/composer/installed.json M  basic/getbasic/e/e.php M  basic/getbasic/error/error.php M  basic/getbasic/inc/inc.php M  basic/getbasic/migrate/Migration.php M  basic/getbasic/migrate/migrate.php M  basic/getbasic/model/model.php M  basic/getbasic/segment/segment.php M  basic/getbasic/view/view.php M  bin/export.php M  bin/migrate.php M  config/db.php M  controller/index.php M  controller/sites.php M  controller/teste.php M  model/inc/limites.php M  model/sites.php M  museu/controller/um.php M  museu/encontrarLimites.php M  museu/salvarRanges.php M  public/.htaccess M  public/index.php M  public/js/canvasjs.min.js M  public/test.php A  sites.txt M  sobre M  table/ranges M  table/sites M  view/form/sites.php M  view/graph.php M  view/inc/header.php M  view/inc/single.php M  view/index.php M  view/test.php

--- controller/teste.php
+++ controller/teste.php
@@ -1,24 +1,69 @@
 <?php
-$single=$_GET['single'];
-if($single==5){
-    header('HTTP/1.0 404 Not Found');
-}else{
-    print $single;
+inc([
+    'db',
+    'model'
+]);
+
+
+function getHttpCode($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent:  Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"));
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 3000);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_NOBODY  , true);  // we don't need body
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_exec($ch);
+    $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch); // Don't forget to close the connection
+    return $response;
 }
-sleep(2);
+
+function httpOk($url){
+    $url='http://'.$url;
+    $code=getHttpCode($url);
+    if($code=="200"){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function httpsOk($url){
+    $url='https://'.$url;
+    $code=getHttpCode($url);
+    if($code=="200"){
+        return true;
+    }else{
+        return $code;
+    }
+}
+
+// if($single==5){
+
+// }else{
+//     print $single;
+// }
+// sleep(2);
 
 //pega o single
 //busca pelo single
 //verifica o status
 //se o status for igual a 200 salva
 //curl "http://cisco.local/teste?single=[1-1000000]" -s
-
-function getHttpCode($url){
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array("User-Agent:  Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0"));
-    $result = curl_exec($ch);
-    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    return $code;
+set_time_limit(3);
+model("sites");
+$single=$_GET['single'];
+$url=readSingle($single)['site'];
+$url=trim($url);
+if(httpOk($url) || httpsOk($url)){
+    $filename=ROOT.'sites.txt';
+    $data=$single.PHP_EOL;
+    file_put_contents($filename,$data,FILE_APPEND);
+    print $single.chr(9).'200 '.$url;
+}else{
+    header('HTTP/1.0 404 Not Found');
+    print $single.chr(9).'404 '.$url;
 }
 ?>
