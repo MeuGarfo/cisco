0dd6f3423037838a48f5f8e66515fc69aa13acef
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-03-07 10:00:37 -0300

    R  "controller/tuplas simples.php" -> controller/tuplas-simples.php

--- controller/tuplas-simples.php
+++ controller/tuplas-simples.php
@@ -8,12 +8,14 @@ set_time_limit(3);
 model("sites");
 
 $sites=file_get_contents(ROOT.'sites.txt');
-$site=explode(PHP_EOL,$sites);
+$sites=explode(PHP_EOL,$sites);
 foreach ($sites as $id_do_site) {
     $url=readSingle($id_do_site)['site'];
     $url=trim($url);
     if(count(explode('.',$url))==2){
-        $filename=ROOT.''
+        $filename=ROOT.'tuplas simples.txt';
+        $data=$url.PHP_EOL;
         file_put_contents($filename,$data,FILE_APPEND);
+        print $url.'<br>';
     }
 }
